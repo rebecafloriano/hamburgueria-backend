902daf66a07fc8f2dfa2edc6284affaa32c613c1
nomeRebeca <rebecaoliver09@gmail.com>
2026-08-07 09:54:13 +0100

    feat: adiciona helmet para headers de seguranca

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,14 @@ import produtoRoutes from './routes/produto.routes';
 import carrinhoRoutes from './routes/carrinho.routes'
 import pedidoRoutes from './routes/pedido.routes'
 import { tratarErro } from './middlewares/erro.middleware';
+import helmet from 'helmet';
 
 
 const app = express();
 
+app.use(helmet())
 app.use(express.json());
+
 
 app.use('/health', healthRoutes);
 app.use('/auth', authRoutes);
